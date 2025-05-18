969d3a5908e89327541d86f0a09b17d49ef81310
Iyaan Azeez <iyaanazeez757@gmail.com>
2025-05-18 21:43:34 +0500

    Move Serialize abstract class into a new module I think Serialize class is going to be implemented by many classes, therefore is its seperate thing.

---
+++ serialization/serialize.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Self
+
+class Serializable(ABC):
+    """ This class should be inherited by all the classes which
+    implement a message protocol that will be send on the blockchain
+    network.
+    """
+    struct = None
+    def __init__(self) -> None:
+        if self.struct is None:
+            raise TypeError("A derived class of Serializable must have struct")
+
+    @abstractmethod
+    def Serialize(self) -> bytes:
+        pass
+
+    @abstractmethod
+    def Deserialize(self, buffer: bytes) -> Self:
+        pass
