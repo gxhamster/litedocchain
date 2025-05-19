63859b2fd8acd9d51653cb0a4fc04dc6d78c311c
Iyaan Azeez <iyaanazeez757@gmail.com>
2025-05-19 22:53:26 +0500

    Added: Async client & server to node Rewrote the logger. The node client will now request peer for blocks when started from its list of peers. Added CLI arguments for specifying ports and addresses. Node server will now handle GETBLOCKMSG sent by peers requesting for blocks. TODO: Need to send Inv message with the hashes of the requested blocks. TODO: Need to transfer the actual blocks too

--- node.py
+++ node.py
@@ -1,22 +1,28 @@
-import socket
 import asyncio
-import time
+import logging
+import threading
+import argparse
 from primitives.block import Block
 from primitives.chain import Chain
-from net.message import MAGIC_HDR_VALUE, MsgHdr, BlockDataMsg, MsgType
+from net.message import MAGIC_HDR_VALUE, MsgHdr, BlockDataMsg, MsgType, GetBlocksMsg
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.DEBUG)
 
 class NetNode:
-    def __init__(self, address: str, port: int = 3333) -> None:
-        self.address = address
-        self.port = port
-        self.peers = []
-        self.clients = []
+    def __init__(self, servAddr: str, servPort: int = 3333) -> None:
+        self.servAddr = servAddr
+        self.servPort = servPort
+        self.peers: list[tuple[asyncio.StreamReader, asyncio.StreamWriter]] = []     # Other connected nodes
+        self.clients = []   # Users connected to this node
         self.chain = Chain()
         
     async def asyncServerCallback(self, reader, writer):
         data = await reader.read(2048)
-        addr = writer.get_extra_info('peername')
-        if data == b'':
+        addr = writer.get_extra_info("peername")
+        logging.debug(f"Connected by: {addr}")
+        if data == b"":
             pass
         if data.startswith(MAGIC_HDR_VALUE):
             hdrSize = MsgHdr.struct.size
@@ -25,51 +31,124 @@ class NetNode:
             if hdr.command == MsgType.NOMSG:
                 print(hdr.command)
             elif hdr.command == MsgType.GETBLOCKMSG:
-                print(hdr.command)
+                logging.debug("MsgType.GETBLOCKMSG [peer -> node]")
+                getBlockMsg = GetBlocksMsg()
+                getBlockMsg = getBlockMsg.Deserialize(data)
+                startIdx = self.chain.GetBlockIdx(getBlockMsg.highestHash)
+                endIdx = self.chain.GetBlockIdx(getBlockMsg.stoppingHash)
+                if endIdx is None or getBlockMsg.stoppingHash.startswith(b'0' * 32):
+                    endIdx = len(self.chain) - 1
+                if startIdx is None:
+                    logging.debug("MsgType.GETBLOCKMSG: Cannot determine range ({startIdx}, {endIdx})")
+                    print(getBlockMsg)
+                else:
+                    if getBlockMsg.highestHash == self.chain.GetLastBlock().hdr.hash:
+                        logging.debug("MsgType.GETBLOCKMSG: Two nodes have same blockchain, ignore")
+                    else:
+                        blocksToSend = []
+                        for idx, block in enumerate(self.chain):
+                            if idx > startIdx and idx <= endIdx:
+                                blocksToSend.append(block)
+                            
+                        logging.debug(f"MsgType.GETBLOCKMSG: Sending {len(blocksToSend)}")
+                        # Send an Inv message with all the serialized blocks packed together
+                            
+                
             elif hdr.command == MsgType.BLOCKDATAMSG:
                 # Received blocks from clients
                 # Verify the block contents are correct
                 # Mine the block
                 # Add the block to localchain
                 # Broadcast block to other nodes if available
-                print(hdr.command)
+                logging.debug("MsgType.BLOCKDATAMSG [client -> node]")
                 bMsg = BlockDataMsg()
                 payload = bMsg.Deserialize(data)
                 if not payload.block.IsBlockValid(check_sig=True):
-                    print("Received an invalid block from a client!")
+                    logging.debug("Received an invalid block from a client!")
                 else:
                     payload.block.MineBlock()
+                    logging.debug(f"Mined block: nonce = {payload.block.hdr.nonce}")
                     self.chain.AddBlockToChain(payload.block)
                     if not self.chain.CheckChainIntegrity():
-                        print("Chain integrity checks failed")
+                        logging.debug("Chain integrity checks failed")
                         # Remove block from chain
                         self.chain.localChain.pop()
                     else:
-                        writer.write(f"Added block {self.chain.GetLastBlock().hdr.hash}".encode())
-                    
-                    
+                        writer.write(
+                            f"Added block {self.chain.GetLastBlock().hdr.hash}".encode()
+                        )
+                        logging.debug(f"Added block: {self.chain.GetLastBlock().hdr.hash.hex()}")
             else:
-                print("Invalid MsgType")
+                logging.debug("Invalid MsgType")
                 assert "Invalid MsgType" == 0
 
-        writer.write(data)
-        await writer.drain()
+        # writer.write(data)
+        # await writer.drain()
         await writer.wait_closed()
-    
+
     async def asyncServer(self):
         server = await asyncio.start_server(
-        self.asyncServerCallback, self.address, self.port)
+            self.asyncServerCallback, self.servAddr, self.servPort
+        )
 
-        addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
-        print(f'Serving on {addrs}')
+        addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+        logging.debug(f"Serving on {addrs}")
 
         async with server:
             await server.serve_forever()
-            
+
     def runAsyncServer(self):
         asyncio.run(self.asyncServer())
+
+    async def asyncClient(self, peerAddr: str, peerPort: int):
+        reader, writer = await asyncio.open_connection(
+            peerAddr, peerPort)
+
+        self.peers.append((reader, writer))
+        
+        if len(self.peers) <= 0:
+            logging.debug("No available peers yet")
+            writer.close()
+            
+        getBlockMsg = GetBlocksMsg()
+        getBlockMsg.highestHash = self.chain.GetLastBlock().hdr.hash
+        getBlockMsg.stoppingHash = b'' * 32
+        getBlockMsg.SetChecksumSize()
+        peerWriterSock = self.peers[-1][1]  # For now just select last peer
+        peerReaderSock = self.peers[-1][0] 
+        peerWriterSock.write(getBlockMsg.Serialize())
+        logging.debug(f"Sent GetBlockMsg to {peerWriterSock.get_extra_info("peername")}")
+        data = peerReaderSock.read()
+        if data == b'':
+            return
+        
+        await writer.wait_closed()
+
+    def runAsyncClient(self, peerAddr: str, peerPort: int):
+        asyncio.run(self.asyncClient(peerAddr, peerPort))
     
+
 if __name__ == "__main__":
-    n = NetNode("localhost")
+    parser = argparse.ArgumentParser(
+        prog='node',
+        description="""Node program for litedocchain""",
+    )
+    parser.add_argument('-a', '--addr', help='node server address', default='localhost')
+    parser.add_argument('-p', '--port', type=int, help='node server port', default=3333)
+    parser.add_argument('--paddr', help='peer address')
+    parser.add_argument('--pport', type=int, help='peer port')
+    args = parser.parse_args()
+    print(args)
+    
+    n = NetNode(args.addr, args.port)
     n.chain.CreateGenesisBlock()
-    n.runAsyncServer()
+    logging.debug(f"Added genesis block: {n.chain.GetGenesisBlock().hdr.hash}")
+    servThread = threading.Thread(target=n.runAsyncServer)
+    servThread.start()
+    
+    if args.paddr is None or args.pport is None:
+        logging.debug("Cannot connect to any peer, no peer address and port")
+    else:
+        n.runAsyncClient(args.paddr, args.pport)
+        
+    servThread.join()
