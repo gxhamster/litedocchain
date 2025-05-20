427d4a1c9d23a6180d52ecfdcbb6e8824a72fe55
Iyaan Azeez <iyaanazeez757@gmail.com>
2025-05-20 23:28:25 +0500

    Prevent connection from waiting for close Previously the node connections only read one message and waited for the socket to be closed. A loop was added.

--- node.py
+++ node.py
@@ -2,95 +2,123 @@ import asyncio
 import logging
 import threading
 import argparse
-from primitives.block import Block
+import dataclasses
+import socket
 from primitives.chain import Chain
-from net.message import MAGIC_HDR_VALUE, MsgHdr, BlockDataMsg, MsgType, GetBlocksMsg, InvMsg
+from net.message import MAGIC_HDR_VALUE, MsgHdr, BlockDataMsg, MsgType, GetBlocksMsg, InvMsg, VersionMsg, VersionConnType
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.DEBUG)
 
+@dataclasses.dataclass
+class Conn:
+    addr: str
+    port: int
+    reader: asyncio.StreamReader
+    writer: asyncio.StreamWriter
+
 class NetNode:
     def __init__(self, servAddr: str, servPort: int = 3333) -> None:
         self.servAddr = servAddr
         self.servPort = servPort
-        self.peers: list[tuple[asyncio.StreamReader, asyncio.StreamWriter]] = []     # Other connected nodes
-        self.clients = []   # Users connected to this node
+        self.peers: list[Conn] = []     # Other connected nodes
+        self.clients: list[Conn] = []   # Users connected to this node
         self.chain = Chain()
         
-    async def asyncServerCallback(self, reader, writer):
-        data = await reader.read(2048)
+    async def asyncServerCallback(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         addr = writer.get_extra_info("peername")
         logging.debug(f"Connected by: {addr}")
-        if data == b"":
-            logging.debug("Peer/Client probably sent an EOF")
-        if data.startswith(MAGIC_HDR_VALUE):
-            hdrSize = MsgHdr.struct.size
-            hdr = MsgHdr()
-            hdr = hdr.Deserialize(data[:hdrSize])
-            if hdr.command == MsgType.NOMSG:
-                print(hdr.command)
-            elif hdr.command == MsgType.GETBLOCKMSG:
-                logging.debug("MsgType.GETBLOCKMSG [peer -> node]")
-                getBlockMsg = GetBlocksMsg()
-                getBlockMsg = getBlockMsg.Deserialize(data)
-                startIdx = self.chain.GetBlockIdx(getBlockMsg.highestHash)
-                endIdx = self.chain.GetBlockIdx(getBlockMsg.stoppingHash)
-                if endIdx is None or getBlockMsg.stoppingHash.startswith(b'0' * 32):
-                    endIdx = len(self.chain) - 1
-                if startIdx is None:
-                    logging.debug("MsgType.GETBLOCKMSG: Cannot determine range ({startIdx}, {endIdx})")
-                    print(getBlockMsg)
-                else:
-                    if getBlockMsg.highestHash == self.chain.GetLastBlock().hdr.hash:
-                        logging.debug("MsgType.GETBLOCKMSG: Two nodes have same blockchain, ignore")
+        while True:
+            data = await reader.read(2048)
+            if data == b"":
+                logging.debug("Peer/Client probably sent an EOF")
+                break
+            if data.startswith(MAGIC_HDR_VALUE):
+                hdrSize = MsgHdr.struct.size
+                hdr = MsgHdr()
+                hdr = hdr.Deserialize(data[:hdrSize])
+                if hdr.command == MsgType.NOMSG:
+                    print(hdr.command)
+                elif hdr.command == MsgType.GETBLOCKMSG:
+                    logging.debug("Received: msg=MsgType.GETBLOCKMSG, dir=[peer -> node]")
+                    getBlockMsg = GetBlocksMsg()
+                    getBlockMsg = getBlockMsg.Deserialize(data)
+                    startIdx = self.chain.GetBlockIdx(getBlockMsg.highestHash)
+                    endIdx = self.chain.GetBlockIdx(getBlockMsg.stoppingHash)
+                    if endIdx is None or getBlockMsg.stoppingHash.startswith(b'0' * 32):
+                        # No stopping limit (get full chain)
+                        endIdx = len(self.chain) - 1
+                    if startIdx is None:
+                        logging.debug("MsgType.GETBLOCKMSG: Cannot determine range ({startIdx}, {endIdx})")
                     else:
-                        blocksToSend = []
-                        for idx, block in enumerate(self.chain):
-                            if idx > startIdx and idx <= endIdx:
-                                blocksToSend.append(block)
+                        if getBlockMsg.highestHash == self.chain.GetLastBlock().hdr.hash:
+                            logging.debug("MsgType.GETBLOCKMSG: Two nodes have same blockchain, ignore")
+                        else:
+                            blocksToSend = []
+                            for idx, block in enumerate(self.chain):
+                                if idx > startIdx and idx <= endIdx:
+                                    blocksToSend.append(block)
+                                
+                            # Send an Inv message with all the serialized blocks packed together
+                            invMsg = InvMsg()
+                            invMsg.blockCount = len(blocksToSend)
+                            invMsg.blocks = blocksToSend
+                            logging.debug(f"Sending: msg=MsgType.InvMsg, dir=[node -> peer], blocks={len(blocksToSend)}")
+                            writer.write(invMsg.Serialize())
+                            await writer.drain()
                             
-                        # Send an Inv message with all the serialized blocks packed together
-                        invMsg = InvMsg()
-                        invMsg.blockCount = len(blocksToSend)
-                        invMsg.blocks = blocksToSend
-                        logging.debug(f"MsgType.InvMsg: [node -> peer] {len(blocksToSend)}")
-                        writer.write(invMsg.Serialize())
-                        await writer.drain()
-                        
-            elif hdr.command == MsgType.BLOCKDATAMSG:
-                # Received blocks from clients
-                # Verify the block contents are correct
-                # Mine the block
-                # Add the block to localchain
-                # Broadcast block to other nodes if available
-                logging.debug("MsgType.BLOCKDATAMSG [client -> node]")
-                bMsg = BlockDataMsg()
-                payload = bMsg.Deserialize(data)
-                if not payload.block.IsBlockValid(check_sig=True):
-                    logging.debug("Received an invalid block from a client!")
-                else:
-                    payload.block.MineBlock()
-                    logging.debug(f"Mined block: nonce = {payload.block.hdr.nonce}")
-                    self.chain.AddBlockToChain(payload.block)
-                    if not self.chain.CheckChainIntegrity(check_sig=True):
-                        logging.debug("Chain integrity checks failed")
-                        # Remove block from chain
-                        self.chain.localChain.pop()
+                elif hdr.command == MsgType.BLOCKDATAMSG:
+                    # Received blocks from clients
+                    logging.debug("Received: msg=MsgType.BLOCKDATAMSG, dir=[client -> node]")
+                    bMsg = BlockDataMsg()
+                    payload = bMsg.Deserialize(data)
+                    if not payload.block.IsBlockValid(check_sig=True):
+                        # Verify the block contents are correct
+                        logging.debug("Received an invalid block from a client!")
                     else:
-                        logging.debug(f"Added block: {self.chain.GetLastBlock().hdr.hash.hex()}")
-                        invMsg = InvMsg()
-                        invMsg.blockCount = 1
-                        invMsg.blocks.append(self.chain.GetLastBlock())
-                        logging.debug(f"MsgType.InvMsg: [node -> peer]: 1 block from client")
-                        # writer.write(invMsg.Serialize())
-                        # await writer.drain()
-            else:
-                logging.debug("Invalid MsgType")
-                assert "Invalid MsgType" == 0
+                        # Mine the block
+                        # Add the block to localchain
+                        payload.block.MineBlock()
+                        logging.debug(f"Mined block: nonce={payload.block.hdr.nonce}")
+                        self.chain.AddBlockToChain(payload.block)
+                        if not self.chain.CheckChainIntegrity(check_sig=True):
+                            logging.debug("Chain integrity checks failed")
+                            # Remove block from chain
+                            self.chain.localChain.pop()
+                        else:
+                            logging.debug(f"Added block: hash={self.chain.GetLastBlock().hdr.hash.hex()}")
+                            invMsg = InvMsg()
+                            invMsg.blockCount = 1
+                            invMsg.blocks.append(self.chain.GetLastBlock())
+                            # Broadcast block to connected peers if available
+                            logging.debug(f"Sending: msg=MsgType.InvMsg (Broadcast), peers={len(self.peers)}")
+                            for peer in self.peers:
+                                peer.writer.write(invMsg.Serialize())
+                                await peer.writer.drain()
+                                logging.debug(f"MsgType.InvMsg: peer={peer.addr}, peer_port={peer.port}")
+                            
+                elif hdr.command == MsgType.VERSION:
+                    verMsg = VersionMsg()
+                    verMsg.Deserialize(data)
+                    sockAddr = socket.inet_ntoa(verMsg.connAddr)
+                    if verMsg.connType == VersionConnType.CLIENT:
+                        logging.debug(f"Received: msg=MsgType.VERSION, type=CLIENT")
+                        self.clients.append(Conn(sockAddr, verMsg.connPort, reader, writer))
+                    elif verMsg.connType == VersionConnType.NODE:
+                        self.peers.append(Conn(sockAddr, verMsg.connPort, reader, writer))
+                        logging.debug(f"MsgType.VERSION: type=NODE")
+                    else:
+                        logging.debug("MsgType.VERSION: connType=UNKNOWN")
+                    
+                        
+                else:
+                    logging.debug("Invalid MsgType")
+                    assert "Invalid MsgType" == 0
 
         # writer.write(data)
         # await writer.drain()
+        writer.close()
         await writer.wait_closed()
 
     async def asyncServer(self):
@@ -99,7 +127,7 @@ class NetNode:
         )
 
         addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-        logging.debug(f"Serving on {addrs}")
+        logging.debug(f"Running node: addr={addrs}")
 
         async with server:
             await server.serve_forever()
@@ -110,49 +138,62 @@ class NetNode:
     async def asyncClient(self, peerAddr: str, peerPort: int):
         reader, writer = await asyncio.open_connection(
             peerAddr, peerPort)
-
-        self.peers.append((reader, writer))
+        logging.debug(f"Connecting: peer_addr={peerAddr}, peer_port={peerPort}")
+        self.peers.append(Conn(peerAddr, peerPort, reader, writer))
         
         if len(self.peers) <= 0:
             logging.debug("No available peers yet")
             writer.close()
+            
+        # Send a VERSION msg
+        verMsg = VersionMsg()
+        verMsg.connType = VersionConnType.NODE
+        sockInfo = writer.get_extra_info("sockname")
+        verMsg.connAddr = socket.inet_aton(sockInfo[0])
+        verMsg.connPort = sockInfo[1]
+        writer.write(verMsg.Serialize())
+        await writer.drain()
+        logging.debug(f"Sending: msg=MsgType.VERSION, type=NODE")
         
         # Initiate the IBD (Initial Block Download)  
         getBlockMsg = GetBlocksMsg()
         getBlockMsg.highestHash = self.chain.GetLastBlock().hdr.hash
         getBlockMsg.stoppingHash = b'' * 32
         getBlockMsg.SetChecksumSize()
-        peerWriterSock = self.peers[-1][1]  # For now just select last peer
-        peerReaderSock = self.peers[-1][0] 
+        peerWriterSock = self.peers[-1].writer  # For now just select last peer
+        peerReaderSock = self.peers[-1].reader
         peerWriterSock.write(getBlockMsg.Serialize())
-        logging.debug(f"Sent GetBlockMsg to {peerWriterSock.get_extra_info("peername")}")
+        logging.debug(f"Sending: msg=GetBlockMsg, peer_addr={peerWriterSock.get_extra_info("peername")[0]}, peer_port={peerWriterSock.get_extra_info("peername")[1]}")
+        
         # Note(iyaan): Maybe hardcoding the reading size is not ideal
         # What happens when we are reading a large InvMsg?
-        data = await peerReaderSock.read(2048)
-        if data == b'':
-            logging.debug("Server probably sent an EOF")
-        elif data.startswith(MAGIC_HDR_VALUE):
-            hdrSize = MsgHdr.struct.size
-            hdr = MsgHdr()
-            hdr = hdr.Deserialize(data[:hdrSize])
-            if hdr.command == MsgType.INVMSG:
-                logging.debug("Received MsgType.InvMsg")
-                invMsg = InvMsg()
-                invMsg.Deserialize(data)
-                for block in invMsg.blocks:
-                    # Find the block which matches the hashPrev
-                    targetIdx = -1
-                    for idx, localBlock in enumerate(self.chain):
-                        if localBlock.hdr.hash == block.hdr.hashPrevBlock:
-                            targetIdx = idx
-                            break
-                    if targetIdx == -1:
-                        logging.debug(f"Cannot add block: {block.hdr.hash.hex()}")
-                    else:
-                        self.chain.localChain.insert(targetIdx + 1, block)
-                        logging.debug(f"Added block: {block.hdr.hash.hex()}")
-                        self.chain.CheckChainIntegrity(check_sig=True)
-        
+        while True:
+            data = await peerReaderSock.read(2048)
+            if data == b'':
+                logging.debug("Server probably sent an EOF")
+                break
+            elif data.startswith(MAGIC_HDR_VALUE):
+                hdrSize = MsgHdr.struct.size
+                hdr = MsgHdr()
+                hdr = hdr.Deserialize(data[:hdrSize])
+                if hdr.command == MsgType.INVMSG:
+                    logging.debug("Received: msg=MsgType.InvMsg")
+                    invMsg = InvMsg()
+                    invMsg.Deserialize(data)
+                    for block in invMsg.blocks:
+                        # Find the block which matches the hashPrev
+                        targetIdx = -1
+                        for idx, localBlock in enumerate(self.chain):
+                            if localBlock.hdr.hash == block.hdr.hashPrevBlock:
+                                targetIdx = idx
+                                break
+                        if targetIdx == -1:
+                            logging.debug(f"Cannot add block: {block.hdr.hash.hex()}")
+                        else:
+                            self.chain.localChain.insert(targetIdx + 1, block)
+                            logging.debug(f"Added block: {block.hdr.hash.hex()}")
+                            self.chain.CheckChainIntegrity(check_sig=True)
+        writer.close()
         await writer.wait_closed()
 
     def runAsyncClient(self, peerAddr: str, peerPort: int):
@@ -173,7 +214,7 @@ if __name__ == "__main__":
     
     n = NetNode(args.addr, args.port)
     n.chain.CreateGenesisBlock()
-    logging.debug(f"Added genesis block: {n.chain.GetGenesisBlock().hdr.hash}")
+    logging.debug(f"Added genesis block: hash={n.chain.GetGenesisBlock().hdr.hash.hex()}")
     servThread = threading.Thread(target=n.runAsyncServer)
     servThread.start()
     
