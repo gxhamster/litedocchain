afd2574fb14089b8960832abdbe68ed8a73f47b9
Iyaan Azeez <iyaanazeez757@gmail.com>
2025-05-21 20:00:19 +0500

    Handle argument errors much better

--- client.py
+++ client.py
@@ -4,28 +4,8 @@ from primitives.block import Block
 from hashlib import sha256
 import argparse
 import socket
+import sys
                     
-def run():
-    private_key = ReadPrivateKeyFromFile()
-    private_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.Raw,
-        format=serialization.PrivateFormat.Raw,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    print("Bytes: ", private_bytes)
-    hexprivate = private_bytes.hex()
-    print("Bytes (hex): ", hexprivate)
-    print("Bytes: ", bytes.fromhex(hexprivate))
-
-    public_key = private_key.public_key()
-    public_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.Raw,
-        format=serialization.PublicFormat.Raw
-    )
-
-    print(public_bytes)
-    print(public_bytes.hex())
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog='client',
@@ -42,54 +22,72 @@ if __name__ == "__main__":
     if args.verify:
         raise NotImplementedError("Verifcation is not yet implemented")
     
-    priv_key = ReadPrivateKeyFromFile()
-    sig2 = FileSigInc(priv_key, args.file)
+    keyfile_name = ''
+    if not args.keyfile:
+        keyfile_name = DEFAULT_KEY_FILE
+    else:
+        keyfile_name = args.keyfile 
     
-    fileHash = b''
-    with open(args.file, 'rb') as file:
-        data = file.read()
-        VerifySig(priv_key, sig2, data)
-        fileHash = sha256(data).digest()
+    if args.generate:
+        try:
+            gen_priv_key(args.keyfile)
+            print(f'Created key file, name={keyfile_name}')
+        except OSError as err:
+            print(f'Cannot create key file, name={keyfile_name}', err)
+        
+    priv_key = import_prv_key_file(args.keyfile)
+    if not priv_key:
+        print(f'Cannot import private key file, name={keyfile_name}')
+        sys.exit(-1)
     
-    if args.addr and args.port:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((args.addr, args.port))
-            
-            verMsg = VersionMsg()
-            verMsg.connType = VersionConnType.CLIENT
-            verMsg.connAddr = socket.inet_aton(sock.getsockname()[0])
-            verMsg.connPort = sock.getsockname()[1]
-            sock.sendall(verMsg.Serialize())
-            
-            print(f"Connected to: node_addr={sock.getpeername()[0]}, node_port={sock.getpeername()[1]}")
-            block = Block()
-            block.pubkey = priv_key.public_key().public_bytes_raw()
-            block.signature = sig2
-            block.fileHash = fileHash
-            block.hdr.hash =  block.hdr.CalculateHash(block.signature + block.fileHash + block.pubkey)
-            if not block.IsBlockValid(check_sig=True):
-                raise AssertionError("Block is not valid")
-            
-            msg = BlockDataMsg()
-            msg.block = block
-            msg.hdr.checksum = msg.CalculateChecksum()
-            msg.hdr.size = len(msg.block.Serialize())
-            
-            sock.sendall(msg.Serialize())
-            print(f"Sending: file='{args.file}', node={sock.getpeername()[0]}")
-            data = sock.recv(1024)
-            blockCreated = False
-            if data.startswith(MAGIC_HDR_VALUE):
-                hdrSize = MsgHdr.struct.size
-                hdr = MsgHdr()
-                hdr = hdr.Deserialize(data[:hdrSize])
-                if hdr.command == MsgType.ACK:
-                    ackMsg = AckMsg()
-                    ackMsg.Deserialize(data)
-                    if ackMsg.nonce == 12:
-                        print(f"Block created successfully on the network")
-                        blockCreated = True
-            if not blockCreated:
-                print("Something went wrong, could not create the block")
-            sock.close()
+    if args.file:
+        file_sig = compute_file_sig_inc(priv_key, args.file)
+        fileHash = b''
+        with open(args.file, 'rb') as file:
+            data = file.read()
+            verify_sig_from_file(priv_key, file_sig, data)
+            fileHash = sha256(data).digest()
+        
+        if args.addr and args.port:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((args.addr, args.port))
+                
+                verMsg = VersionMsg()
+                verMsg.connType = VersionConnType.CLIENT
+                verMsg.connAddr = socket.inet_aton(sock.getsockname()[0])
+                verMsg.connPort = sock.getsockname()[1]
+                sock.sendall(verMsg.Serialize())
+                
+                print(f"Connected to: node_addr={sock.getpeername()[0]}, node_port={sock.getpeername()[1]}")
+                block = Block()
+                block.pubkey = priv_key.public_key().public_bytes_raw()
+                block.signature = file_sig
+                block.fileHash = fileHash
+                block.hdr.hash =  block.hdr.CalculateHash(block.signature + block.fileHash + block.pubkey)
+                if not block.IsBlockValid(check_sig=True):
+                    raise AssertionError("Block is not valid")
+                
+                
+                block_msg = BlockDataMsg()
+                block_msg.block = block
+                block_msg.hdr.checksum = block_msg.CalculateChecksum()
+                block_msg.hdr.size = len(block_msg.block.Serialize())
+                sock.sendall(block_msg.Serialize())
+                
+                print(f"Sending: file='{args.file}', node={sock.getpeername()[0]}")
+                data = sock.recv(1024)
+                block_created = False
+                if data.startswith(MAGIC_HDR_VALUE):
+                    hdrSize = MsgHdr.struct.size
+                    hdr = MsgHdr()
+                    hdr = hdr.Deserialize(data[:hdrSize])
+                    if hdr.command == MsgType.ACK:
+                        ackMsg = AckMsg()
+                        ackMsg.Deserialize(data)
+                        if ackMsg.nonce == 12:
+                            print(f"Block created successfully on the network")
+                            block_created = True
+                if not block_created:
+                    print("Something went wrong, could not create the block")
+                sock.close()
         
